28992b6ee303f1b403eccb330d612ed9675d2456
Sebastian Niklasch <s.niklasch@gmx.de>
2013-01-10 15:03:33 +0100

    Möglichkeit Eier zur Karte hinzuzufügen erstellt
    Es können nun über die Funktion addEgg Eier zur Karte hinzugefügt
    werden.

--- js/map_big.js
+++ js/map_big.js
@@ -4,3 +4,15 @@ L.tileLayer('http://{s}.tile.cloudmade.com/cc2b230c7e24424eb2d4b2928fceba79/997/
 	attribution: 'Kartendaten &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Bilddaten &copy; <a href="http://cloudmade.com">CloudMade</a>',
 	maxZoom: 18
 }).addTo(map);
+
+function addEgg(lat, lon, feedID){
+	var eggIcon = L.icon({
+		iconUrl: 'img/eggicon.png'
+	});
+	var eggMarker = L.marker([lat, lon], {icon: eggIcon}, {title: feedID}).addTo(map);
+	eggMarker.on('click', function(e){
+			alert(feedID);
+		});
+}
+
+addEgg(51.962944, 7.628694, 1337);
