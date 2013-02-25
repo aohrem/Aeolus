9f000430c81da9f4b00d8b33d3afdcf5ccf7aa8e
Anja Schlaphorst <a.schlaphorst@wwu.de>
2013-02-25 13:53:05 +0100

    Map Bugfix
    Alle AQEs werden auf der Karte angezeigt

--- map.inc.php
+++ map.inc.php
@@ -1,14 +1,16 @@
 <?php
 	$db = new Sql();
-	if ($db->num_rows('SELECT `feed_id` FROM `egg`') == 0) {
+	$query = mysql_query('SELECT `feed_id`,`lat`,`lon` FROM `egg`');
+	if (mysql_num_rows($query) == 0) {
 		$this->contentTemplate->cleanCode('Egg');
 	}
 	else {
-		$row = $db->fetch('SELECT `feed_id`,`lat`,`lon` FROM `egg` WHERE `feed_id`=56789');
- 		$this->contentTemplate->copyCode('Egg');
-		$this->contentTemplate->tplReplaceOnce('egg_lat', $row->lat);
-		$this->contentTemplate->tplReplaceOnce('egg_lon', $row->lon);
-		$this->contentTemplate->tplReplaceOnce('egg_fid', $row->feed_id);
+		while ($row = mysql_fetch_object($query)) {
+ 			$this->contentTemplate->copyCode('Egg');
+			$this->contentTemplate->tplReplaceOnce('egg_lat', $row->lat);
+			$this->contentTemplate->tplReplaceOnce('egg_lon', $row->lon);
+			$this->contentTemplate->tplReplaceOnce('egg_fid', $row->feed_id);
+		}
 		$this->contentTemplate->cleanCode('Egg');
 	}
 ?>
