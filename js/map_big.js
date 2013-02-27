c6c55ab87c4feb0a7cd0263774b7b3ae97e4578c
Sebastian Niklasch <s.niklasch@gmx.de>
2013-02-27 15:01:08 +0100

    Vorbereitung zur Anzeige der LANUV-Daten

--- js/map_big.js
+++ js/map_big.js
@@ -5,9 +5,15 @@ L.tileLayer('http://{s}.tile.cloudmade.com/cc2b230c7e24424eb2d4b2928fceba79/997/
 	maxZoom: 18
 }).addTo(map);
 
+<<<<<<< HEAD
 function addEgg(lat, lon, feedID, type, color){
 	var eggIcon = L.icon({
 				iconUrl: 'img/' + type + '_' + color + '.png'
+=======
+function addEgg(lat, lon, feedID, color, type){
+	var eggIcon = L.icon({
+				iconUrl: 'img/' + type + 'icon_' + color + '.png'
+>>>>>>> Vorbereitung zur Anzeige der LANUV-Daten
 			});
 	var eggMarker = L.marker([lat, lon], {icon: eggIcon}, {title: feedID}).addTo(map);
 	eggMarker.on('click', function(e){
