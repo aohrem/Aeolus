fbbf2973b81f34acf8c3391a339439694c633daa
Anja Schlaphorst <a.schlaphorst@wwu.de>
2013-03-04 14:44:42 +0100

    AQEs auf der Karte klassifizieren
    - Auswahl der Buttons auf der Karte
    - Einblenden einer Legende

--- map.inc.php
+++ map.inc.php
@@ -1,4 +1,19 @@
 <?php
+	// find URL of the current page
+	$url = $_SERVER['REQUEST_URI'];
+	$url_parts = explode('/', $url);
+    $url = $url_parts[sizeof($url_parts)-1];
+	// delete former input for classify in URL
+	if (isset($_GET['classify'])){
+		$classifier = htmlentities(mysql_real_escape_string($_GET['classify']));
+		$this->contentTemplate->tplReplace('show', 'class="show"');
+		$this->contentTemplate->tplReplace('classifier', $classifier);
+		$url = str_replace('&classify='.$classifier,'',$url);
+	}
+	else {
+		$this->contentTemplate->tplReplace('show', '');
+	}
+	
 	$db = new Sql();
 	$query = mysql_query('SELECT `feed_id`,`lat`,`lon` FROM `egg`');
 	if (mysql_num_rows($query) == 0) {
@@ -10,8 +25,16 @@
 			$this->contentTemplate->tplReplaceOnce('egg_lat', $row->lat);
 			$this->contentTemplate->tplReplaceOnce('egg_lon', $row->lon);
 			$this->contentTemplate->tplReplaceOnce('egg_fid', $row->feed_id);
-			$this->contentTemplate->tplReplaceOnce('egg_color', "'noval'");
+			// Check if classification shoud be shown
+			if (isset($_GET['classify'])) {
+				// distinction of classes for classifying
+				$this->contentTemplate->tplReplaceOnce('egg_color', "'class1'");
+			}
+			else {
+				$this->contentTemplate->tplReplaceOnce('egg_color', "'noval'");
+			}
 		}
 		$this->contentTemplate->cleanCode('Egg');
 	}
+	$this->contentTemplate->tplReplace('url', $url);
 ?>
