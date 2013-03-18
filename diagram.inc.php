331e70de1dccef88292e01d89382200978f4f2fb
Andreas Ohrem <andreas.ohrem@googlemail.com>
2013-03-18 17:34:50 +0100

    Ausreißererkennung im Diagramm

--- diagram.inc.php
+++ diagram.inc.php
@@ -80,28 +80,52 @@
             
 			// check if outliers shall be interpolated
 			if ( isset($_GET['interpolateoutliers']) && $_GET['interpolateoutliers'] == 'true' && $sensitivity != 0 ) {
+                
 				$interpolateOutliers = true;
 				$this->contentTemplate->tplReplace('interpolateOutliers', 'true');
-                $this->contentTemplate->tplReplace('outlierState', translate('outlier_state_interpolated'));
+                $this->contentTemplate->tplReplace('outlierState', translate('outlier_state_interpolated_diagram'));
+                $this->contentTemplate->tplReplace('coOutlierTable', '');
+                $this->contentTemplate->tplReplace('no2OutlierTable', '');
+                $this->contentTemplate->tplReplace('temperatureOutlierTable', '');
+                $this->contentTemplate->tplReplace('humidityOutlierTable', '');
+                
 				$dataArray = $dataValidation->interpolateOutliers($outliers);
 				
-				$tplOutliers = '<a href="index.php?s=diagram&amp;fid='.$feedId.'&amp;timeframe='.$timeframe.'&amp;interpolateoutliers=false&amp;lang='.$this->language.'"><span class="bigoutlier interpolated success" onMouseOver="outlierNote(\'outliers_interpolated\');" onMouseOut="outlierNote(\'outliers_interpolated\');">i</span></a><div id="outliers_interpolated" class="bigoutlierhint interpolated">'.translate('outliers_interpolated').'</div>';
+				$tplOutliers = '<a href="index.php?s=diagram&amp;fid='.$feedId.'&amp;timeframe='.$timeframe.'&amp;interpolateoutliers=false&amp;lang='.$this->language.'"><span class="bigoutlier interpolated success" onMouseOver="outlierNote(\'outliers_interpolated\');" onMouseOut="outlierNote(\'outliers_interpolated\');">i</span></a><div id="outliers_interpolated" class="bigoutlierhint interpolated">'.translate('outliers_interpolated_diagram').'</div>';
 			}
 			else {
 				$interpolateOutliers = false;
 				$this->contentTemplate->tplReplace('interpolateOutliers', 'false');
-                $this->contentTemplate->tplReplace('outlierState', translate('outlier_state_marked'));
-				
+                $this->contentTemplate->tplReplace('outlierState', translate('outlier_state_marked_diagram'));
+                
+                if ( $sensitivity != 0 ) {
+                    foreach ( $outliers as $sensor => $val ) {
+                        $i = 0;
+                        $outlierTable[$sensor] = '';
+                        foreach ( $val as $time => $out ) {
+                            if ( $out == 1 ) {
+                                $outlierTable[$sensor] .= '{column:1, row:'.$i.'}, ';
+                            }
+                            $i++;
+                        }
+                        $outlierTable[$sensor] = substr($outlierTable[$sensor], 0, -2);
+                        $this->contentTemplate->tplReplace($sensor.'OutlierTable', $outlierTable[$sensor]);
+                    }
+                }
+                else {
+                    $this->contentTemplate->tplReplace('coOutlierTable', '');
+                    $this->contentTemplate->tplReplace('no2OutlierTable', '');
+                    $this->contentTemplate->tplReplace('temperatureOutlierTable', '');
+                    $this->contentTemplate->tplReplace('humidityOutlierTable', '');
+                }
+                
 				// check if dataset contains outliers
 				if ( $containsOutliers && $sensitivity != 0 ) {
-					$tplOutliers = '<a href="index.php?s=diagram&amp;fid='.$feedId.'&amp;timeframe='.$timeframe.'&amp;interpolateoutliers=true&amp;lang='.$this->language.'"><span class="bigoutlier error" onMouseOver="outlierNote(\'outliers_found\');" onMouseOut="outlierNote(\'outliers_found\');">!</span></a><div id="outliers_found" class="bigoutlierhint">'.translate('outliers_found').'</div>';
+					$tplOutliers = '<a href="index.php?s=diagram&amp;fid='.$feedId.'&amp;timeframe='.$timeframe.'&amp;interpolateoutliers=true&amp;lang='.$this->language.'"><span class="bigoutlier error" onMouseOver="outlierNote(\'outliers_found\');" onMouseOut="outlierNote(\'outliers_found\');">!</span></a><div id="outliers_found" class="bigoutlierhint">'.translate('outliers_found_diagram').'</div>';
 				}
 			}
 			
 			$this->contentTemplate->tplReplace('outliers', $tplOutliers);
-            
-            
-            
             
             // iterate sensor data
             $i = 1;
@@ -115,18 +139,55 @@
 				// copy table row and fill in sensor data for one timestamp
 				$this->contentTemplate->copyCode('diagramData');
                 $this->contentTemplate->tplReplaceOnce('t', date('Y, m-1, d, H, i', $time));
+                
                 $this->contentTemplate->tplReplaceOnce('co', $val['co']);
                 $this->contentTemplate->tplReplaceOnce('lt', date('d.m.Y, g:i a', $time));
                 $this->contentTemplate->tplReplaceOnce('co', $val['co']);
+                
+                if ( $outliers['co'][$time] && ! $interpolateOutliers && $sensitivity != 0 ) {
+                    $coOutlierText = '<br><br>'.translate('value_could_be_an_outlier');
+                }
+                else {
+                    $coOutlierText = '';
+                }
+                $this->contentTemplate->tplReplaceOnce('co_outlier_text', $coOutlierText);
+                
                 $this->contentTemplate->tplReplaceOnce('no2', $val['no2']);
                 $this->contentTemplate->tplReplaceOnce('lt', date('d.m.Y, g:i a', $time));
                 $this->contentTemplate->tplReplaceOnce('no2', $val['no2']);
+                
+                if ( $outliers['no2'][$time] && ! $interpolateOutliers && $sensitivity != 0 ) {
+                    $no2OutlierText = '<br>'.translate('value_could_be_an_outlier');
+                }
+                else {
+                    $no2OutlierText = '';
+                }
+                $this->contentTemplate->tplReplaceOnce('no2_outlier_text', $no2OutlierText);
+                
                 $this->contentTemplate->tplReplaceOnce('temp', $val['temperature']);
                 $this->contentTemplate->tplReplaceOnce('lt', date('d.m.Y, g:i a', $time));
                 $this->contentTemplate->tplReplaceOnce('temp', $val['temperature']);
+                
+                if ( $outliers['temperature'][$time] && ! $interpolateOutliers && $sensitivity != 0 ) {
+                    $tempOutlierText = '<br>'.translate('value_could_be_an_outlier');
+                }
+                else {
+                    $tempOutlierText = '';
+                }
+                $this->contentTemplate->tplReplaceOnce('temp_outlier_text', $tempOutlierText);
+                
                 $this->contentTemplate->tplReplaceOnce('hum', $val['humidity']);
                 $this->contentTemplate->tplReplaceOnce('lt', date('d.m.Y, g:i a', $time));
                 $this->contentTemplate->tplReplaceOnce('hum', $val['humidity']);
+                
+                if ( $outliers['humidity'][$time] && ! $interpolateOutliers && $sensitivity != 0 ) {
+                    $humOutlierText = '<br>'.translate('value_could_be_an_outlier');
+                }
+                else {
+                    $humOutlierText = '';
+                }
+                $this->contentTemplate->tplReplaceOnce('hum_outlier_text', $humOutlierText);
+                
                 $this->contentTemplate->tplReplaceOnce('timestamp', $time);
                 
                 // check if it's the last entry, if true, delete the last comma from at the end of the data table row
@@ -140,11 +201,6 @@
 			}
 			// delete the last row
 			$this->contentTemplate->cleanCode('diagramData');
-            
-            
-            
-            
-            
         }
 		// handle errors if parsing the xml was not successfull
 		else {
