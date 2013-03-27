80a20419fc3320dd3f0da1dbb8d863b6c3745698
Sebastian Niklasch <s.niklasch@gmx.de>
2013-03-27 14:16:19 +0100

    Kreismenu Handhabung angepasst

--- js/map_big.js
+++ js/map_big.js
@@ -1,4 +1,5 @@
 var map = L.map('map_big').setView([51.962944, 7.628694,], 13);	
+var bgMarker, checkMarker, diagMarker, tableMarker, menuDisplayed = false;
 
 L.tileLayer('http://{s}.tile.cloudmade.com/cc2b230c7e24424eb2d4b2928fceba79/997/256/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Image data &copy; <a href="http://cloudmade.com">CloudMade</a>',
@@ -18,6 +19,10 @@ function addEgg(lat, lon, feedID, type, color, sensor){
 }
 
 function openCircleMenu(lat, lon, feedID){
+		if (menuDisplayed == true){
+			removeCircleMenu();
+			menuDisplayed = false;
+		}
 		var bgIcon = L.icon({
 			iconUrl:	'img/kreismenu_hintergrund.png',
 			iconAnchor:	[75, 75],
@@ -38,49 +43,35 @@ function openCircleMenu(lat, lon, feedID){
 			iconAnchor:	[75, 15],
 			iconSize:	[72, 90]
 		});
-		var closeIcon = L.icon({
-			iconUrl:	'img/close.png',
-			iconSize:	[10, 10],
-			iconAnchor:	[5, 5]
-		});
-		var bgMarker = new L.Marker([lat, lon], {
+		bgMarker = new L.Marker([lat, lon], {
 				icon: bgIcon,
 				zIndexOffset: 200
 			}).addTo(map);
-		var checkMarker = new L.Marker([lat, lon], {
+		checkMarker = new L.Marker([lat, lon], {
 				icon: checkIcon,
 				zIndexOffset: 200
 			}).addTo(map);
-		var tableMarker = new L.Marker([lat, lon], {
+		tableMarker = new L.Marker([lat, lon], {
 				icon: tableIcon,
 				zIndexOffset: 200
 			}).addTo(map);
-		var diagMarker = new L.Marker([lat, lon], {
+		diagMarker = new L.Marker([lat, lon], {
 				icon: diagIcon,
 				zIndexOffset: 200
 			}).addTo(map);
-		var closeMarker = new L.Marker([lat, lon], {
-				icon: closeIcon,
-				zIndexOffset: 200
-			}).addTo(map);
+		
+		menuDisplayed = true;
 		
 		checkMarker.on('click', function(e){ alert("check" + feedID) });
 		diagMarker.on('click', function(e){ location.href='index.php?s=diagram&fid=' + feedID + '&lang=de';   });
 		tableMarker.on('click', function(e){ location.href='index.php?s=table&fid=' + feedID + '&timeframe=6h&interpolateoutliers=false&sensitivity=2&lang=de'; });
-		closeMarker.on('click', function(e){
-				map.removeLayer(bgMarker);
-				map.removeLayer(checkMarker);
-				map.removeLayer(diagMarker);
-				map.removeLayer(tableMarker);
-				map.removeLayer(closeMarker);
-			});
 }
 
-function addPopup(lat, lon, type){
-	var popup = L.popup()
-		.setLatLng([lat, lon])
-		//.setContent(type)
-		.openOn(map);
+function removeCircleMenu(){
+	map.removeLayer(bgMarker);
+	map.removeLayer(checkMarker);
+	map.removeLayer(diagMarker);
+	map.removeLayer(tableMarker);
 }
 
-//addPopup(51.962944, 7.628694, "Hallo");
+map.on('click', removeCircleMenu);
