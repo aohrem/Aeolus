c376c76871b8236e25917589d074b9842475c0f8
Sebastian Niklasch <s.niklasch@gmx.de>
2013-04-02 12:17:27 +0200

    Download Button Kreismenu
    Der Check-Button wurde durch einen Download-Button ersetzt, durch
    welchen man nun direkt zur Download-Seite kommt.

--- js/map_big.js
+++ js/map_big.js
@@ -1,5 +1,5 @@
 var map = L.map('map_big').setView([51.962944, 7.628694,], 13);	
-var bgMarker, checkMarker, diagMarker, tableMarker, menuDisplayed = false;
+var bgMarker, downloadMarker, diagMarker, tableMarker, menuDisplayed = false;
 
 L.tileLayer('http://{s}.tile.cloudmade.com/cc2b230c7e24424eb2d4b2928fceba79/997/256/{z}/{x}/{y}.png', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Image data &copy; <a href="http://cloudmade.com">CloudMade</a>',
@@ -24,31 +24,31 @@ function openCircleMenu(lat, lon, feedID){
 			menuDisplayed = false;
 		}
 		var bgIcon = L.icon({
-			iconUrl:	'img/kreismenu_hintergrund.png',
+			iconUrl:	'img/kreismenu.png',
 			iconAnchor:	[75, 75],
 			iconSize:	[150, 150]
 		});
-		var checkIcon = L.icon({
-			iconUrl:	'img/kreismenu_check.png',
-			iconAnchor:	[35, 75],
-			iconSize:	[70, 55]
+		var downloadIcon = L.icon({
+			iconUrl:	'img/kreismenu_download.png',
+			iconAnchor:	[30, -15],
+			iconSize:	[60, 60]
 		});
 		var tableIcon = L.icon({
 			iconUrl:	'img/kreismenu_tabelle.png',
-			iconAnchor:	[-3, 15],
-			iconSize:	[72, 90]
+			iconAnchor:	[0, 75],
+			iconSize:	[75, 90]
 		});
 		var diagIcon = L.icon({
 			iconUrl:	'img/kreismenu_diagramm.png',
-			iconAnchor:	[75, 15],
-			iconSize:	[72, 90]
+			iconAnchor:	[75, 75],
+			iconSize:	[75, 90]
 		});
 		bgMarker = new L.Marker([lat, lon], {
 				icon: bgIcon,
 				zIndexOffset: 200
 			}).addTo(map);
-		checkMarker = new L.Marker([lat, lon], {
-				icon: checkIcon,
+		downloadMarker = new L.Marker([lat, lon], {
+				icon: downloadIcon,
 				zIndexOffset: 200
 			}).addTo(map);
 		tableMarker = new L.Marker([lat, lon], {
@@ -62,14 +62,14 @@ function openCircleMenu(lat, lon, feedID){
 		
 		menuDisplayed = true;
 		
-		checkMarker.on('click', function(e){ alert("check" + feedID) });
+		downloadMarker.on('click', function(e){ location.href='index.php?s=download&fid=' + feedID + '&timeframe=6h&interpolateoutliers=false&sensitivity=2&lang=de';   });
 		diagMarker.on('click', function(e){ location.href='index.php?s=diagram&fid=' + feedID + '&lang=de';   });
 		tableMarker.on('click', function(e){ location.href='index.php?s=table&fid=' + feedID + '&timeframe=6h&interpolateoutliers=false&sensitivity=2&lang=de'; });
 }
 
 function removeCircleMenu(){
 	map.removeLayer(bgMarker);
-	map.removeLayer(checkMarker);
+	map.removeLayer(downloadMarker);
 	map.removeLayer(diagMarker);
 	map.removeLayer(tableMarker);
 }
