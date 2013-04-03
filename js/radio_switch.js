8b801f4320b2d594adcb87594ca798bc3c0c01c5
Anja Schlaphorst <a.schlaphorst@wwu.de>
2013-04-03 12:14:25 +0200

    JS-Codeübersicht verbessert

--- js/radio_switch.js
+++ js/radio_switch.js
@@ -1,10 +1,10 @@
 function radioSwitch(radio) {
-    var radioButton = document.getElementById(radio);
+	var radioButton = document.getElementById(radio);
 
-    if (radioButton.checked) {
-        radioButton.checked = false;
-    }
-    else {
-        radioButton.checked = true;
-    }
+	if (radioButton.checked) {
+		radioButton.checked = false;
+	}
+	else {
+		radioButton.checked = true;
+	}
 }
