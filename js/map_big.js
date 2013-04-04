1c0e67e843a7d07f0d3c96b711d6f9fa04e7244f
Andreas Ohrem <andreas.ohrem@googlemail.com>
2013-04-04 10:16:26 +0200

    Kartenzoom angepasst

--- js/map_big.js
+++ js/map_big.js
@@ -1,4 +1,4 @@
-var map = L.map('map_big').setView([51.962944, 7.628694,], 13);	
+var map = L.map('map_big').setView([51.962944, 7.628694,], 5);	
 var bgMarker, downloadMarker, diagMarker, tableMarker, menuDisplayed = false;
 
 L.tileLayer('http://{s}.tile.cloudmade.com/cc2b230c7e24424eb2d4b2928fceba79/997/256/{z}/{x}/{y}.png', {
