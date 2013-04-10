51b2d50a707e5c7671f99a3c464b566de876f300
Anja Schlaphorst <a.schlaphorst@wwu.de>
2013-04-10 19:46:30 +0200

    Konsistenz Sprache Startseitenkarte

--- js/map_small.js
+++ js/map_small.js
@@ -2,7 +2,7 @@ var map = L.map('map_small', { keyboard: false }).setView([51.962944, 7.628694, 
 	
 
 L.tileLayer('http://{s}.tile.cloudmade.com/cc2b230c7e24424eb2d4b2928fceba79/997/256/{z}/{x}/{y}.png', {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Bilddaten &copy; <a href="http://cloudmade.com">CloudMade</a>',
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Image data &copy; <a href="http://cloudmade.com">CloudMade</a>',
 	maxZoom: 18
 }).addTo(map);
 
